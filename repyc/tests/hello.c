6a08b2c6a5abea56fcf3dfcfa3bc7df3ca6601bf
cmatthew <cmatthew@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2010-12-07 21:26:56 +0000

    I new tiny tester.

---
+++ repyc/tests/hello.c
@@ -0,0 +1,26 @@
+/*
+ * hello.c
+ *
+ *  Created on: 2010-12-07
+ *      Author: cmatthew
+ */
+
+#include <repy.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int rc = 0;
+
+	rc = repy_init();
+	if (rc) {
+		printf("Problem loading repy: %d", rc);
+		return rc;
+	} else {
+		printf("Repy Says: %s", getmyip());
+
+		repy_shutdown();
+	}
+	return 0;
+}
+
