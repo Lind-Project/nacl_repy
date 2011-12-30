dcb3e65415d53c22eb6a30abddd381bf81d35c86
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2011-12-30 23:39:10 +0000

    fixing a bug in the multiple open call

--- seattlelib/tests/ut_lind_fs_multiple_open.py
+++ seattlelib/tests/ut_lind_fs_multiple_open.py
@@ -5,22 +5,20 @@ from lind_fs_constants import *
 
 # Try read / write of a file and see if it works...
 lind_fs_calls._blank_fs_init()
-print "1"
 myfd = lind_fs_calls.open_syscall('/foo',O_CREAT | O_EXCL | O_RDWR,S_IRWXA)
 
-print "2"
 myfd2 = lind_fs_calls.open_syscall('/foo',O_RDWR,S_IRWXA)
 
-print "3"
-assert(myfd== myfd2)
+assert(myfd!= myfd2)
 
 flags = 577
 mode = 438
 name = 'double_open_file'
 
-print "CM: failing double open here:"
+#print "CM: failing double open here:"
 myfd3 = lind_fs_calls.open_syscall(name, flags, mode)
+lind_fs_calls.write_syscall(myfd3,'hi there')
 
 myfd4 = lind_fs_calls.open_syscall(name, flags, mode)
 
-assert(myfd3==myfd4)
+assert(myfd3!=myfd4)
