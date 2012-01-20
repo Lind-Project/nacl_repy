f0163b5a1a949c7ef044b83cdf653d78eeb20239
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-01-20 19:56:13 +0000

    fixing repyportability issue related to getresources call which is impacting one of the fs calls.

--- seattlelib/tests/fs_test_wrapper.py
+++ seattlelib/tests/fs_test_wrapper.py
@@ -22,6 +22,19 @@ nanny.tattle_quantity = do_nothing
 nanny.tattle_add_item = do_nothing
 nanny.tattle_remove_item = do_nothing
 
+nanny._resources_allowed_dict = {'cpu':0, 'messport':set(range(50000,60000)),
+      'connport': set(range(50000,60000)), 'memory':0, 'diskused':0,
+      'filewrite': 0, 'fileread':0, 'netsend':0, 'netrecv':0,
+      'loopsend': 0, 'looprecv':0, 'lograte':0, 'random':0,
+      'events':set(), 'filesopened':set(), 'insockets':set(), 'outsockets':set()
+      }
+nanny._resources_consumed_dict = {'messport':set(), 'connport':set(), 'cpu':0, 
+      'memory':0, 'diskused':0,
+      'filewrite': 0, 'fileread':0, 'netsend':0, 'netrecv':0,
+      'loopsend': 0, 'looprecv':0, 'lograte':0, 'random':0,
+      'events':set(), 'filesopened':set(), 'insockets':set(), 'outsockets':set()
+      }
+
 from lind_fs_constants import *
 
 
