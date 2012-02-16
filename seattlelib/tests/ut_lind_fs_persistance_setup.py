be95a7dbfb5e7fb557fccdd067fd9bd355a2f0bb
cmatthew <cmatthew@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-02-16 22:09:20 +0000

    updated the persistence test to account for a new type of persistence bug, that is a crash that happens when the inodes are fewer than there used to be.

--- seattlelib/tests/ut_lind_fs_persistance_setup.py
+++ seattlelib/tests/ut_lind_fs_persistance_setup.py
@@ -14,9 +14,12 @@ import os
 from lind_fs_constants import *
 
 TEST_FILENAME = "/someSimpleFileName"
+TEST2_FILENAME = "/otherSimpleFileName2"
+
 
 
 if __name__ == '__main__':
+
     lind_fs_calls._blank_fs_init()
 
     # Everything is okay, so now make a file
@@ -42,6 +45,18 @@ if __name__ == '__main__':
     assert(lind_fs_calls.read_syscall(myfd, 100) == 'hello world!')
 
     lind_fs_calls.close_syscall(myfd)
+
+    # Now make and remove a file:
+    myfd = lind_fs_calls.open_syscall(TEST2_FILENAME, \
+                                      O_CREAT | O_EXCL | O_RDWR, S_IRWXA)
+
+    # write should succeed
+    message = '================================================================================================================================'
+    assert(lind_fs_calls.write_syscall(myfd, message) == len(message))
+
+    lind_fs_calls.close_syscall(myfd)
+
+    lind_fs_calls.unlink_syscall(TEST2_FILENAME)
 
     lind_fs_calls.persist_metadata(DEFAULT_METADATA_FILENAME)
 
