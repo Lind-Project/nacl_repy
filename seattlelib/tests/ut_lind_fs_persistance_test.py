0f8106f822d9083610bdc17270647c7edd53c260
cmatthew <cmatthew@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-02-20 20:45:57 +0000

    Uncomitted error in persistence test. Make sure that if the file is there, we pass the test, not fail with exception.

--- seattlelib/tests/ut_lind_fs_persistance_test.py
+++ seattlelib/tests/ut_lind_fs_persistance_test.py
@@ -14,18 +14,18 @@ import os
 from lind_fs_constants import *
 from ut_lind_fs_persistance_setup import TEST_FILENAME
 
+SyscallError = lind_fs_calls.SyscallError
+
 print "Found Metadata? ", os.access(DEFAULT_METADATA_FILENAME, os.W_OK)
 
 lind_fs_calls.restore_metadata(DEFAULT_METADATA_FILENAME)
 
 # Everything is okay, so now make a file
 
-myfd = lind_fs_calls.open_syscall(TEST_FILENAME, \
-                                  O_CREAT | O_EXCL | O_RDWR, S_IRWXA)
-
-
-assert(lind_fs_calls.read_syscall(myfd, 100) == 'hello world!')
-
-lind_fs_calls.close_syscall(myfd)
-
-lind_fs_calls.persist_metadata(DEFAULT_METADATA_FILENAME)
+try:
+    myfd = lind_fs_calls.open_syscall(TEST_FILENAME, \
+                                     O_CREAT | O_EXCL | O_RDWR, S_IRWXA)
+except SyscallError:
+    pass
+else:
+    assert False, "This file should exist!"
