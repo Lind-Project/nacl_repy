fb7e54c7652a7b45820b28f8c7756ec696e216bd
cmatthew <cmatthew@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-04-18 23:20:06 +0000

    This test was not working at all. The blind catches were catching socket errors, not the write errors we were expecting.
    I have changed the file descriptors so that the connection happen correctly.  I have removed some of the blind catches. I have reformatted the code as well to fix up some styleistic issues.

--- seattlelib/tests/ut_lind_net_shutdown.py
+++ seattlelib/tests/ut_lind_net_shutdown.py
@@ -6,7 +6,7 @@ from emulmisc import exitall
 
 from time import sleep
 
-from lind_net_constants import *
+from lind_net_constants import AF_INET, SOCK_STREAM, SHUT_RDWR
 
 SyscallError = lind_test_server.SyscallError
 
@@ -18,26 +18,25 @@ clientsockfd = lind_test_server.socket_syscall(AF_INET, SOCK_STREAM, 0)
 
 
 # bind to a socket
-lind_test_server.bind_syscall(serversockfd,'127.0.0.1',50431)
+lind_test_server.bind_syscall(serversockfd, '127.0.0.1', 50431)
 
-lind_test_server.listen_syscall(serversockfd,10)
+lind_test_server.listen_syscall(serversockfd, 10)
+
 
 def do_server():
-  
-  try:
     newsocketfd = lind_test_server.accept_syscall(serversockfd)
-    lind_test_server.send_syscall(serversockfd,'jhasdfhjsa',0)
-    lind_test_server.shutdown_syscall(serversockfd,SHUT_RDWR)
+    fd = newsocketfd[2]
+    lind_test_server.send_syscall(fd, 'jhasdfhjsa', 0)
+    lind_test_server.setshutdown_syscall(fd, SHUT_RDWR)
 
     try:
-      # should fail!!!
-      lind_test_server.send_syscall(serversockfd,'jhasdfhjsa',0)
-    except: 
-      pass
+        # should fail!!!
+        lind_test_server.send_syscall(fd, 'jhasdfhjsa', 0)
+    except:
+        pass
     else:
-      print "send after shutdown didn't fail!!!"
+        print "send after shutdown didn't fail!!!"
 
-  finally:
     exitall()
 
 
@@ -48,10 +47,7 @@ createthread(do_server)
 sleep(.1)
 
 # should be okay...
-try:
-  lind_test_server.connect_syscall(clientsockfd,'127.0.0.1',50431)
-  assert(lind_test_server.getpeername_syscall(clientsockfd) == ('127.0.0.1',50431))
-finally:
-  exitall()
 
-
+lind_test_server.connect_syscall(clientsockfd, '127.0.0.1', 50431)
+assert(lind_test_server.getpeername_syscall(clientsockfd) == \
+       ('127.0.0.1', 50431))
