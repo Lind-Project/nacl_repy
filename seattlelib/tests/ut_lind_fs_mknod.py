1e5bfc171d4b940c0ed891009a6d0debd811d6a8
nitin <nitin@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-08-31 23:16:44 +0000

    unit tests for mknod and socketpair.

---
+++ seattlelib/tests/ut_lind_fs_mknod.py
@@ -0,0 +1,41 @@
+"""
+File: ut_lind_fs_mknod.py
+
+Unit test for mknod_syscall(), which is used to create special files.
+"""
+
+import lind_test_server
+from lind_fs_constants import *
+
+lind_test_server._blank_fs_init()
+
+# let's create /dev/null...
+lind_test_server.mknod_syscall('/null', S_IFCHR, (1, 3))
+fd = lind_test_server.open_syscall('/null', O_CREAT | O_RDWR, S_IRWXA)
+
+assert lind_test_server.fstat_syscall(fd)[2] & S_FILETYPEFLAGS == S_IFCHR,\
+  "File should be a Character file."
+assert lind_test_server.fstat_syscall(fd)[6] == (1, 3),\
+  "File is not /dev/null."
+assert lind_test_server.write_syscall(fd, "test") == 4,\
+  "Write failed to /dev/null file."
+assert lind_test_server.read_syscall(fd, 10) == '',\
+  "Read failed from /dev/null file."
+
+lind_test_server.close_syscall(fd)
+
+
+# let's create /dev/random...
+lind_test_server.mknod_syscall('/random', S_IFCHR, (1, 8))
+fd = lind_test_server.open_syscall('/random', O_CREAT | O_RDWR, S_IRWXA)
+
+assert lind_test_server.fstat_syscall(fd)[2] & S_FILETYPEFLAGS == S_IFCHR,\
+  "File should be a Character file."
+assert lind_test_server.fstat_syscall(fd)[6] == (1, 8),\
+  "File is not /dev/random."
+assert lind_test_server.write_syscall(fd, "test") == 4,\
+  "Write failed to /dev/random file."
+assert lind_test_server.read_syscall(fd, 10) != '',\
+  "Read failed from /dev/random file."
+
+lind_test_server.close_syscall(fd)
---
+++ seattlelib/tests/ut_lind_net_socketpair.py
@@ -0,0 +1,57 @@
+"""
+File : ut_lind_net_socketpair.py
+
+Unit test for socketpair_syscall(), it's used to check if a pair of sockets
+are returned successfully with two-way communication. Works for both TCP/UDP.
+"""
+
+import lind_test_server
+import emultimer
+
+from lind_net_constants import *
+
+SyscallError = lind_test_server.SyscallError
+
+# other end of communicator...
+def helper():
+  msg = None
+  msg = lind_test_server.recv_syscall(sockets[0], 1024, 0)
+  assert msg != None, "Sockpair recv failed in helper..."
+
+  lind_test_server.send_syscall(sockets[0], "SocketPair test.", 0)
+
+  return
+
+# one end of communicator...
+def tester():
+  # need simultaneous communication...
+  emultimer.createthread(helper)
+  emultimer.sleep(0.1)
+
+  lind_test_server.send_syscall(sockets[1], "SocketPair test.", 0)
+
+  msg = None
+  msg = lind_test_server.recv_syscall(sockets[1], 1024, 0)
+  assert msg != None, "Sockpair recv failed in tester..."
+
+  emultimer.sleep(0.1)
+
+  lind_test_server.close_syscall(sockets[1])
+  lind_test_server.close_syscall(sockets[0])
+
+  return
+
+# Let's get a piar of sockets and check if a two way communication
+# is possible. This is TCP sockpair.
+sockets = []
+lind_test_server.socketpair_syscall(AF_INET, SOCK_STREAM, 0, sockets)
+
+# performs message passing among the sockets...
+tester()
+
+# Test for UDP connection...
+sockets = []
+lind_test_server.socketpair_syscall(AF_INET, SOCK_DGRAM, 0, sockets)
+
+# performs message passing among the sockets...
+tester()
