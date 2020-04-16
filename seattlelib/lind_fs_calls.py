d97a7e0fd1fc907c3c1f4dcda853e098ad321f69
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-04-16 15:36:24 -0400

    timing

--- seattlelib/lind_fs_calls.py
+++ seattlelib/lind_fs_calls.py
@@ -110,6 +110,8 @@
 # Store all of the information about the file system in a dict...
 # This should not be 0 because this is considered to be deleted
 
+import time
+
 ROOTDIRECTORYINODE = 1
 STREAMINODE = 2
 
