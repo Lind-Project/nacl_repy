5b1b7ec6839e12862c4917e84ab4d85671431c84
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-08-13 16:12:45 -0400

    disable read nanny

--- repy/emulfile.py
+++ repy/emulfile.py
@@ -431,9 +431,8 @@ class emulated_file (object):
       disk_blocks_read += 1
 
     # Charge 4K per block
-    # nanny.tattle_quantity('fileread', disk_blocks_read*4096)
-    nanny.tattle_quantity('fileread', 0)
-
+    nanny.tattle_quantity('fileread', disk_blocks_read*4096)
+    
 
     # Return the data
     return data
