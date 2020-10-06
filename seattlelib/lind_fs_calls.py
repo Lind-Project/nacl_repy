1f9e481acaef5b1244e4f6c6fb1c409ec7ff20e2
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-10-06 17:20:58 -0400

    log

--- seattlelib/lind_fs_calls.py
+++ seattlelib/lind_fs_calls.py
@@ -210,8 +210,10 @@ def print_log():
     if "fs_call" in curr: total_fs_time += curr["fs_call"]
     print logstring
 
-  print "Total system call time " + str(total_syscall_time * 1000000) + " us"
-  print "Total pure implementation time " + str(total_fs_time * 1000000) + " us"
+  print str(total_syscall_time * 1000000)
+  print str(total_fs_time * 1000000)
+  # print "Total system call time " + str(total_syscall_time * 1000000) + " us"
+  # print "Total pure implementation time " + str(total_fs_time * 1000000) + " us"
 
 
 ROOTDIRECTORYINODE = 1
