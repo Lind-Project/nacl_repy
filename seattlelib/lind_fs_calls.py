6785980162b325db758c915c2ac41508112d6ce7
jesings <34111484+jesings@users.noreply.github.com>
2020-10-25 10:21:28 -1000

    Fork/exec fix

--- seattlelib/lind_fs_calls.py
+++ seattlelib/lind_fs_calls.py
@@ -2883,6 +2883,7 @@ def get_fs_call(CONST_CAGEID, CLOSURE_SYSCALL_NAME):
       
       master_fs_calls_context[child_cageid] = \
         repy_deepcopy(master_fs_calls_context[CONST_CAGEID])
+      del master_fs_calls_context[child_cageid]['syscall_table']
     
       parentagetable[child_cageid] = {'ppid': CONST_CAGEID}
     
@@ -2914,6 +2915,7 @@ def get_fs_call(CONST_CAGEID, CLOSURE_SYSCALL_NAME):
 
       masterfdlocktable[child_cageid] = createlock()
       
+      del master_fs_calls_context[CONST_CAGEID]['syscall_table']
       master_fs_calls_context[child_cageid] = \
         repy_deepcopy(master_fs_calls_context[CONST_CAGEID])
 
