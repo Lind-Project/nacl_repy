aef757297125b5b1ad23b003098317dc600277c4
Çağlar Doğan <dogancaglar05@gmail.com>
2020-11-29 06:20:07 +0300

    Update lind_net_calls.py

--- seattlelib/lind_net_calls.py
+++ seattlelib/lind_net_calls.py
@@ -1728,15 +1728,17 @@ def get_net_call(CONST_CAGEID,CLOSURE_SYSCALL_NAME):
     
     filedescriptortable = masterfiledescriptortable[CONST_CAGEID]
     
-    print("hello")
+    print("hello, ", DEFAULT_HOSTNAME)
     
     #Using the DEFAULT_HOSTNAME ("Lind") for now since we do not let user change the host name at this point.
     result = DEFAULT_HOSTNAME[0,length]
     
+    print("Result is: ", result)
+    
     if(length < 0):
       raise SyscallError("gethostname_syscall","EINVAL","Invalid argument")
     
-    print(result)
+    print("Confirm: ",result)
     
     return result
   
