4c9ffbcd3afe182b2b05dbba252d24ae99ddd329
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2021-01-18 21:58:37 -0500

    other

--- seattlelib/lind_fs_calls.py
+++ seattlelib/lind_fs_calls.py
@@ -213,7 +213,7 @@ def print_log():
     if "stub" in curr: logstring += " stub time " + str(curr["stub"] * 1000000) + " us"
     if "closure" in curr: logstring += " closure time " + str(curr["closure"] * 1000000) + " us"
     if "fs_call" in curr: logstring += " fs call time " + str(curr["fs_call"] * 1000000) + " us"
-    if "other" in curr: logstring + = " other time " + str(curr["other"] * 1000000) + " us"
+    if "other" in curr: logstring += " other time " + str(curr["other"] * 1000000) + " us"
     total_syscall_time += curr["syscall"]
     if "fs_call" in curr: total_fs_time += curr["fs_call"]
     print logstring
