4a9fb78801fac6532ae028ae3aadb49b61472a6e
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2021-02-23 17:50:55 -0500

    try

--- seattlelib/lind_fs_calls.py
+++ seattlelib/lind_fs_calls.py
@@ -1221,7 +1221,7 @@ class cageobj:
         # this is a regular file.  If it's not open, let's open it!
         if inode not in fileobjecttable:
           thisfo = openfile(FILEDATAPREFIX+str(inode),False)
-          print thisfo.fileno()
+          print thisfo.fileobj.fileno()
           fileobjecttable[inode] = thisfo
 
       # I'm going to assume that if you use O_APPEND I only need to
